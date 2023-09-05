7ff6669277d61017ef34b8d06bd227405dd1e929
Prateek <prateeksinghkamalpura@gmail.com>
2023-09-05 09:57:38 +0530

    first push

---
+++ first.js
@@ -0,0 +1,75 @@
+// console.log("hello world");
+
+// var a=5;
+// console.log(a);
+// let b=6;
+// console.log(b);
+
+// {
+//     let ab=7;
+//     console.log(ab);
+// }
+
+// var a=5;
+// console.log(a);
+// var a="hello";
+// console.log(a);
+
+// let a=5;
+// a="Hello";
+// console.log(a);
+
+// const abc =5;
+// console.log(abc);
+
+//Data Types in javascript
+
+//Null,Number,Boolean,Bigint,String,Symbol,Undefined
+
+// let a= null;
+// let b=5;
+// let c=true;
+// let d="striing";
+// let e= BigInt("234");
+// let f = Symbol("this is symbol");
+// let g = undefined;
+
+// console.log(a,b,c,d,e,f,g);
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// console.log(typeof(f));
+// console.log(typeof(g));
+
+// const student = {
+//     name: "abc",
+//     roll: "50",
+//     sec: "A"
+
+// }
+// console.log(student);
+
+
+//Operator in javascript
+
+//Airthmetic operator
+
+// let a =10;
+// let b=5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+let a = prompt("enter a number")
+
+
+
+
+
+
+
